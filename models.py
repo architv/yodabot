e4af19e1925e880b1d377d1f4ff732517bf6b1b9
Archit Verma <architv07@gmail.com>
2016-04-20 14:12:18 +0530

    deleted all files" "

--- models.py
+++
@@ -1,7 +0,0 @@
-from google.appengine.ext import ndb
-
-class Games(ndb.Model):
-    """Models Contests"""
-    data = ndb.TextProperty(default="{}")
-    created = ndb.DateTimeProperty(auto_now_add=True)
-    modified = ndb.DateTimeProperty(auto_now=True)
