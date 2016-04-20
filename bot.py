6a074cac31f1ffe360d63d44ee9655abc1f7fd44
Archit Verma <architv07@gmail.com>
2016-04-20 13:37:32 +0000

    created code to send message

--- bot.py
+++ bot.py
@@ -1,17 +1,49 @@
 import os
 from flask import Flask
 from flask import request
+import json
+import requests
+
+import config
 
 app = Flask(__name__)
 
-@app.route("/")
+def send_message(sender_id, text):
+    json_data = {
+        "recipient": {"id": sender_id},
+        "message": {"text": text + " to you!"}
+    }
+    params = {
+        "access_token": config.FB_MESSENGER_TOKEN
+    }
+    r = requests.post('https://graph.facebook.com/v2.6/me/messages', json=json_data, params=params)
+    print(r, r.status_code, r.text)
+
+
+@app.route("/", methods=['POST', 'GET'])
 def main():
-	verification_code = 'bot_this_is_of_yoda'
-	verify_token = request.args.get("hub.verify_token")
-	if verification_code == verify_token:
-		return request.args.get("hub.challenge")
+	if request.method == 'GET':
+		verification_code = 'bot_this_is_of_yoda'
+		verify_token = request.args.get("hub.verify_token")
+		if verification_code == verify_token:
+			return request.args.get("hub.challenge")
+		else:
+			return 'hello world'
+
+	elif request.method == 'POST':
+		print 'hello'
+		body = request.data
+		print "BODY" + body
+        messaging_events = json.loads(body)['entry'][0]['messaging']
+        for event in messaging_events:
+        	sender = event['sender']['id']
+        	if event['message'] and event['message']['text']:
+        		text = event['message']['text']
+        		send_message(sender, text)
+		return 'received'
+
 	else:
-		return 'hello world'
+		return 'Error'
 
 
 if __name__ == "__main__":
@@ -20,5 +52,6 @@ if __name__ == "__main__":
 	# variable (that is set in each Heroku deployment environment, or dyno) 
 	# and start the app listening on that port
 	# Read http://virantha.com/2013/11/14/starting-a-simple-flask-app-with-heroku/
+	app.debug = True
 	port = int(os.environ.get("PORT", 8000))
 	app.run(host='0.0.0.0', port=port)
