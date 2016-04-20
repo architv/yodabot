dd5e505bb3683b9087ce72fd109b963754d1b9f4
Archit Verma <architv07@gmail.com>
2016-04-20 12:43:14 +0000

    changed host port

--- bot.py
+++ bot.py
@@ -6,4 +6,5 @@ def main():
 	return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+	port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
