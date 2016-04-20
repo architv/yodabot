804e71aae32c0c143f8d2ae79adc79a8b3998223
Archit Verma <architv07@gmail.com>
2016-04-20 13:50:40 +0000

    some more fixes

--- bot.py
+++ bot.py
@@ -9,42 +9,37 @@ import config
 app = Flask(__name__)
 
 def send_message(sender_id, text):
-    json_data = {
-        "recipient": {"id": sender_id},
-        "message": {"text": text + " to you!"}
-    }
-    params = {
-        "access_token": config.FB_MESSENGER_TOKEN
-    }
-    r = requests.post('https://graph.facebook.com/v2.6/me/messages', json=json_data, params=params)
-    print(r, r.status_code, r.text)
+	json_data = {
+		"recipient": {"id": sender_id},
+		"message": {"text": text + " to you!"}
+	}
+	params = {
+		"access_token": config.FB_MESSENGER_TOKEN
+	}
+	r = requests.post('https://graph.facebook.com/v2.6/me/messages', json=json_data, params=params)
+	print(r, r.status_code, r.text)
 
 
 @app.route("/", methods=['POST', 'GET'])
 def main():
-	if request.method == 'GET':
-		verification_code = 'bot_this_is_of_yoda'
-		verify_token = request.args.get("hub.verify_token")
-		if verification_code == verify_token:
-			return request.args.get("hub.challenge")
-		else:
-			return 'hello world'
+  if request.method == 'GET':
+    verification_code = 'bot_this_is_of_yoda'
+    verify_token = request.args.get("hub.verify_token")
+    if verification_code == verify_token:
+      return request.args.get("hub.challenge")
+    else:
+      return 'hello world'
 
-	elif request.method == 'POST':
-		print 'hello'
-		body = request.data
-		print "BODY" + body
-        messaging_events = json.loads(body)['entry'][0]['messaging']
-        sender = messaging_events[0]['sender']['id']
-        text = messaging_events['message']['text']
-        # for event in messaging_events:
-        # 	sender = event['sender']['id']
-        # 	if event['message'] and event['message']['text']:
-        # 		text = event['message']['text']
-        send_message(sender, text)
-		return 'received'
-
-	else:
+  elif request.method == 'POST':
+    print 'hello'
+    body = request.data
+    print "BODY" + body
+    messaging_events = json.loads(body)['entry'][0]['messaging']
+    sender = messaging_events[0]['sender']['id']
+    text = messaging_events[0]['message']['text']
+    send_message(sender, text)
+    return 'received'
+  else:
 		return 'Error'
 
 
