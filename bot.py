df63f7efd559eab74d722986c72c8c91500235b6
Archit Verma <architv07@gmail.com>
2016-04-20 12:48:56 +0000

    changed the port

--- bot.py
+++ bot.py
@@ -8,5 +8,10 @@ def main():
 	return "Hello World!"
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 8000))
-	app.run(host='0.0.0.0', port=port)
+	# The default port of 5000 that a Flask app starts on does not work
+	# for a Heroku deployment. Instead, you need to get the PORT environment
+	# variable (that is set in each Heroku deployment environment, or dyno) 
+	# and start the app listening on that port
+	# Read http://virantha.com/2013/11/14/starting-a-simple-flask-app-with-heroku/
+	# port = int(os.environ.get("PORT", 8000))
+	app.run(host='0.0.0.0', port=8000)
